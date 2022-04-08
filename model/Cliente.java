955b28bc2e76abbffa24c8affb9fb7fbf87bd66a
Vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2022-04-08 02:04:04 +0000

    Serializando classe

--- model/Cliente.java
+++ model/Cliente.java
@@ -1,6 +1,11 @@
 package model;
 
-public class Cliente {
+import java.io.Serializable;
+
+public class Cliente implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
     private String nome;
     private String cpf;
     private String profissao;
