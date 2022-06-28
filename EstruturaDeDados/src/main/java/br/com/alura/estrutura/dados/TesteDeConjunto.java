c96aa1eb5f580560d2b472098a08fe8bfec08deb
Vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2022-06-28 03:53:14 +0000

    Conjunto

---
+++ EstruturaDeDados/src/main/java/br/com/alura/estrutura/dados/TesteDeConjunto.java
@@ -0,0 +1,28 @@
+package br.com.alura.estrutura.dados;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TesteDeConjunto {
+    public static void main(String[] args) {
+        Conjunto conjunto = new Conjunto();
+        conjunto.adiciona("Mauricio");
+        System.out.println(conjunto);
+
+        conjunto.adiciona("Mauricio");
+        System.out.println(conjunto);
+
+        conjunto.adiciona("Marcelo");
+        conjunto.adiciona("Guilherme");
+        System.out.println(conjunto);
+        
+        conjunto.remove("Mauricio");
+        System.out.println(conjunto);
+
+        /* ------ */
+
+        Set<String> conjuntoDoJava = new HashSet<String>();
+
+        conjuntoDoJava.add("Mauricio");
+    }
+}
