c7fde2f2460c1554be5f2d8aec7d3f27db0abaae
vinicius tertuliano <viniciustertulianodasilva@hotmail.com>
2022-11-23 07:36:24 -0300

    Microsserviços na prática: implementando com Java e Spring

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -0,0 +1,55 @@
+package br.com.alurafood.pagamentos.service;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.model.Status;
+import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository pagamentoRep;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PagamentoDto> obterTodos(Pageable paginacao) {
+        return pagamentoRep
+                .findAll(paginacao)
+                .map(p -> modelMapper.map(p, PagamentoDto.class));
+    }
+
+    public PagamentoDto obterPorId(Long id) {
+        Pagamento pagamento = pagamentoRep.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException());
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto criarPagamento(PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        pagamentoRep.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto atualizarPagamento(Long id, PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setId(id);
+        pagamento = pagamentoRep.save(pagamento);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public void excluirPagamento(Long id) {
+        pagamentoRep.deleteById(id);
+    }
+}
